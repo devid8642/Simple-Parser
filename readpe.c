7e062d517cbba7e6cf4b5375690f0118119eda44
Devid <nnick4673@gmail.com>
2020-12-05 10:38:39 -0300

    Implemented function that makes a simple check on the file.

--- readpe.c
+++ readpe.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void err(char *msg) {
   fprintf(stderr, "%s\n", msg);
   exit(1);
+}
+
+bool ispe(const unsigned char *b) {
+  if (b[0] != 'M' || b[1] != 'Z')
+    return false;
+
+  return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -21,6 +29,9 @@ int main(int argc, char *argv[]) {
   if (fread(buffer, 32, 1, fh) != 1)
     err("Error! I was unable to read the 32 bytes of the file.");
 
+  if (! ispe(buffer))
+    err("Error! Pass an .exe file.");
+  
   return 0;
 
 }
