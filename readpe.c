b574dc4c9daca848c17fbcd94f7bc1c104515796
Devid <nnick4673@gmail.com>
2020-12-05 10:20:30 -0300

    Opening and trying to read the file.

--- readpe.c
+++ readpe.c
@@ -8,9 +8,18 @@ void err(char *msg) {
 
 int main(int argc, char *argv[]) {
   FILE *fh;
+  unsigned char buffer[32];
 
   if (argc != 2)
-    err("Erro! Passe um arquivo .exe para ser lido.");
+    err("Error! Pass an .exe file to be read.");
+
+  fh = fopen(argv[1], "rb");
+
+  if (fh == NULL)
+    err("Error! File not found or cannot be read.");
+
+  if (fread(buffer, 32, 1, fh) != 1)
+    err("Error! I was unable to read the 32 bytes of the file.");
 
   return 0;
 
